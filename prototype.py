5e1bbaadeaf4e214f793310b3c180dc9a049809b
khalil kafrouni <khalil.kafrouni@gmail.com>
2025-09-19 18:16:51 +0200

    modularized data pull

--- prototype.py
+++ prototype.py
@@ -1,4 +1,4 @@
-# Dashboard with equity (SPY), forex (USD), commodities (Gold, crude oil, wheat), bonds (Inflation-linked bonds).
+# Dashboard with equity (SPY), forex (USD), commodities (Gold ($GC=F), crude oil ($WTI, or $USO), wheat ($ZW=F))), bonds ($^TNX) Inflation-linked bonds).
 # Data about growth, inflation, volatility, and yield.
 # Data goes back as far as possible, with widget slider to choose time frame.
 
@@ -9,26 +9,51 @@ import matplotlib.pyplot as plt
 
 # Getting the data for SPY (daily)
 # Closing prices only
-# data = yf.download("SPY")['Close']
-# data.to_csv("spy_data.csv")
-# print(data.index)
+# spy = yf.download("SPY")['Close']
+# spy.to_csv("spy_data.csv")
+# print(spy.index)
 # print('\n\n\n\n')
 
-spy = (pd.read_csv("spy_data.csv", index_col=0, parse_dates=True)['SPY'].pct_change() + 1).cumprod()
+# Data Download
+def data_download(ticker, filename):
+    data = yf.download(ticker)['Close']
+    data.to_csv(filename)
+
+ticker_filename = {
+    "SPY": "spy.csv",
+    "DX-Y.NYB": "usd.csv",
+    "GC=F": "gold.csv",
+    "WTI": "wti.csv",
+    "ZW=F": "wheat.csv",
+    "^TNX": "bonds.csv"
+}
+
+# for ticker, filename in ticker_filename.items():
+#     data_download(ticker, filename)
+
+# Sanity Checks
+# Check for missing values
+def check_na(data):
+    null_sum = data.isna().sum()
+    null_percentage = null_sum/len(data)
+    print(f"Ratio of missing values: {null_percentage}\n Count of missing values: {null_sum}")
+
+
+spy = pd.read_csv('spy.csv', index_col=0, parse_dates=True)
+check_na(spy)
 
 # # Check for missing values
 # missing_values = data.isnull().sum() # number is zero, no missing values
 
 # Plot the data
-spy.plot(label='SPY')
-plt.ylabel("SPY Closing Price")
-plt.title("SPY Closing Price Over Time")
-plt.yscale('log')
+# spy.plot(label='SPY')
+# plt.ylabel("SPY Closing Price")
+# plt.title("SPY Closing Price Over Time")
+# plt.yscale('log')
 
-# forex using USD index
-usd = (yf.download("DX-Y.NYB", start=spy.index.min())['Close'].pct_change() + 1).cumprod()
-usd['DX-Y.NYB'].plot(label='USD Index')
+# # forex using USD index
+# usd['DX-Y.NYB'].plot(label='USD Index')
 
-plt.legend()
-plt.show()
+# plt.legend()
+# plt.show()
 
